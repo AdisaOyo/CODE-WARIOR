7a0d04dc1bdf522a22d559510bb7259e2a57bdfd
Splendid <adedayoadewole761@gmail.com>
2026-07-27 15:51:11 +0100

    Filter out the geese

---
+++ codewars/python/6-kyu/filter-out-the-geese.py
@@ -0,0 +1,17 @@
+geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+def goose_filter(birds):
+    #your code here
+    for x in geese:
+        if x in birds:
+            birds.remove(x)
+    return birds
+import codewars_test as test
+from solution import goose_filter
+
+@test.describe("Fixed Tests")
+def fixed_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(goose_filter(["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]),["Mallard", "Hook Bill", "Crested", "Blue Swedish"])
+        test.assert_equals(goose_filter(["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"]),["Mallard", "Barbary", "Hook Bill", "Blue Swedish", "Crested"])
+        test.assert_equals(goose_filter(["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]),[])
